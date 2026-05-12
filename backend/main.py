48a5111a1ad7e5a47f1b8de330f98e28db21b501
Saumyadip Das <150571773+saumyadip-das-2003@users.noreply.github.com>
2026-05-12 07:21:46 +0600

    Minor Update

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "https://docreaderbd.vercel.app",
         "https://*.vercel.app",
         "*"
     ],
